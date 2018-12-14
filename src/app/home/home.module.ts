ed13f3c6756106c4f50272e0d3359c197e90ed2a
Thiago Felipe Peçanha <thiagofelipepecanha@gmail.com>
2018-12-14 00:58:34 -0200

    Adding ReactiveForms and refactoring routes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,26 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 import { TranslateModule } from '@ngx-translate/core';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  }
+];
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     TranslateModule.forChild(),
-    RouterModule.forChild([{ path: '', component: HomePage }])
+    RouterModule.forChild(routes)
   ],
   declarations: [HomePage]
 })
