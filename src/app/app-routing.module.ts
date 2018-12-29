5ef96dbcea0544a290ad574668762ce0bc17f0f6
Thiago Felipe Peçanha <thiagofelipepecanha@gmail.com>
2018-12-29 00:40:27 -0200

    Home Page with some mocked images and

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from './services/authentication/auth.guard';
-
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  // { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
   { path: 'reset-password', loadChildren: './reset-password/reset-password.module#ResetPasswordPageModule' },
-  { path: 'first-access', loadChildren: './first-access/first-access.module#FirstAccessPageModule' }
+  { path: 'first-access', loadChildren: './first-access/first-access.module#FirstAccessPageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
