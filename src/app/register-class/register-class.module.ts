82b113273a9716f5bc2b9517444036315b8eb217
Thiago Felipe Peçanha <thiagofelipepecanha@gmail.com>
2019-01-29 00:10:34 -0200

    Creating register class feature, catching CodeClubs data from it's API

---
+++ src/app/register-class/register-class.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { RegisterClassPage } from './register-class.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: RegisterClassPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [RegisterClassPage]
+})
+export class RegisterClassPageModule {}
