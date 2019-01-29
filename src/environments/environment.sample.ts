0f398cf65a97a8b1bedeea77d93475a20770065a
Thiago Felipe Peçanha <thiagofelipepecanha@gmail.com>
2019-01-29 00:07:42 -0200

    Adding ENVIRONMENT VARS with Code Club's API data

--- src/environments/environment.sample.ts
+++ src/environments/environment.sample.ts
@@ -4,6 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
+  codeClubApiUri: 'https://api.codeclubworld.org',
+  codeClubApiToken: 'ENTER YOUR API CODE CLUB TOKEN',
   firebase: {
     apiKey: 'ENTER YOUR FIREBASE APIKEY',
     authDomain: 'ENTER YOUR FIREBASE AUTHDOMAIN',
