e0395f5e69dd007fb9e0152143f64c34111f5999
Thiago Felipe Peçanha <thiagofelipepecanha@gmail.com>
2019-03-08 00:19:35 -0300

    Adding User management methods

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,25 +1,78 @@
 import { Injectable } from '@angular/core';
-// import { Storage } from '@ionic/storage';
-import { User } from '../../models/user';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+
+
 export class UserService {
+  public userProfile: firebase.firestore.DocumentReference
+  public currentUser: firebase.User
 
-  constructor() { }
-
-  public insert(user: User) {
-    const key = new Date().getTimezoneOffset.toString();
-    return this.save(key, user);
+  constructor() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.currentUser = user;
+        this.userProfile = firebase.firestore().doc(`/userProfile/${user.uid}`)
+      }
+    })
   }
 
-  private save(key: string, user: User) {
-    // return this.storage.set(key, user);
+  getUserProfile(): firebase.firestore.DocumentReference {
+    return this.userProfile
   }
 
-  public signup(value) {
-    // this.storage.set('email', value.email);
-    // this.storage.set('password', value.matching_passwords.password);
+  updateName(firstName: string, lastName: string): Promise<any> {
+    return this.userProfile.update({ firstName, lastName })
   }
+
+  updateTelephone(newTelephone: string): Promise<any> {
+    return this.userProfile.update({ newTelephone })
+  }
+
+  async updateEmail(newEmail: string, password: string): Promise<any> {
+    const credential: firebase.auth.AuthCredential =
+      firebase.auth.EmailAuthProvider.credential(
+        this.currentUser.email,
+        password
+      )
+
+    try {
+      await this.currentUser
+        .reauthenticateAndRetrieveDataWithCredential(credential)
+      this.currentUser.updateEmail(newEmail).then(() => {
+        this.userProfile.update({ email: newEmail })
+      })
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  async updatePassword(newPassword: string, oldPassword: string): Promise<any> {
+    const credential: firebase.auth.AuthCredential =
+      firebase.auth.EmailAuthProvider.credential(
+        this.currentUser.email,
+        oldPassword
+      )
+
+    try {
+      await this.currentUser
+        .reauthenticateAndRetrieveDataWithCredential(credential)
+      this.currentUser.updatePassword(newPassword).then(() => {
+        this.userProfile.update({ password: newPassword })
+      })
+
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
+
 }
